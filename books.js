d7d7a35d8317d9a5359bf94c62ab885c06d71e01
Christopher Stanley <chstanley@trollandtoad.com>
2024-06-11 08:16:56 -0400

    Updates book directory app
    - Replaces data from JSON file with data from a MongoDB collection
    - Replaces basic object manipulation with MongoDB collection methods.
    - Adds .env file for sensitive data.

--- books.js
+++ books.js
@@ -1,46 +1,138 @@
 const express = require('express');
-const router = express.Router();
-const books = require('./books.json');
+const router = express.Router()
+require('dotenv').config();
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+const db = client.db(process.env.MONOGODB_NAME);
+const books = db.collection(process.env.MONOGODB_COLLECTION);
+
+async function getBooks(id = null){
+    try {
+
+        if (id) {
+            const query = { book_id: parseInt(id) };
+            return await books.findOne(query);
+        }
+
+        return await books.find().toArray();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function addBook(book) {
+    try {
+        let nextId;
+
+        await getNextId().then(id => {
+            nextId = id;
+        });
+
+        const doc = {
+            book_id: nextId,
+            title: book.title,
+            author: book.author,
+        };
+
+        await books.insertOne(doc);
+        return await books.find().toArray();
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function updateBook(id, book) {
+
+    try {
+
+        const filter = { book_id: parseInt(id) };
+
+        const doc = {
+            $set: {
+                title: book.title,
+                author: book.author,
+            }
+        };
+
+        await books.updateOne(filter, doc);
+        return await books.find().toArray();
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function deleteBook(id) {
+    try {
+
+        const filter = { book_id: parseInt(id) };
+
+        await books.deleteOne(filter);
+        return await books.find().toArray();
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function getNextId() {
+    const book = await books.find().sort({book_id: -1}).limit(1).toArray();
+    return book[0].book_id + 1;
+}
+
 
 // Get all books.
 router.get('/', (req, res) => {
-    res.json(books);
+    getBooks().then(books => {
+        res.json(books);
+    });
 });
 
 // Get a specific book by ID.
 router.get('/:id', (req, res) => {
     const id = req.params.id;
-    res.json(books.filter((book) => book.id === parseInt(id)));
+    getBooks(id).then(books => {
+        res.json(books);
+    });
 });
 
 // Add a book.
 router.post('/', (req, res) => {
     const body = req.body;
-    books.push(body);
-    res.json(books);
+    addBook(body).then(books => {
+        res.json(books);
+    });
 });
 
 // Update a book.
 router.put('/:id', (req, res) => {
     const id = req.params.id;
     const body = req.body;
-    books.forEach((book, index) => {
-        if (book.id === parseInt(id)) {
-            books[index] = body;
-        }
+
+    updateBook(id, body).then(book => {
+        res.json(book);
     });
-    res.json(books);
 });
 
 // Remove a book.
 router.delete('/:id', (req, res) => {
     const id = req.params.id;
-    books.forEach((book, index) => {
-        if (book.id === parseInt(id)) {
-            books.splice(index, 1);
-        }
+
+    deleteBook(id).then(book => {
+        res.json(book);
     });
-    res.json(books);
 });
 
 module.exports = router;
